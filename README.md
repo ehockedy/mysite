18dc950a9e5c1853c8e3303e4bd043e2e8d6a35c
ehockedy <edhockedy@hotmail.com>
2022-05-31 22:02:35 +0100

    Initial commit
    Adding repo setup and webpack config.

---
+++ README.md
@@ -0,0 +1,3 @@
+Code for the personal website of Edward Hockedy
+
+Serve by running "yarn start"
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/components/index.tsx",
+    target: "web",  // This builds for web, not servers
+    mode: "development",
+    devServer: {
+        port: 3000
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        // Resolve file extensions in the specified order
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: "css-loader",
+            },
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    // Used to parse React code, and support browsers that use all versions of js/ts
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        // This generates the index.html with all required script tags using the given template
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "public", "index.html"),
+        }),
+        // new MiniCssExtractPlugin({
+        //     filename: "./src/yourfile.css",
+        // }),
+    ],
+};
