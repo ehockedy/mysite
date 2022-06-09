75a0857829a705eb0ff0b32ae1cea16eeb0e804b
ehockedy <edhockedy@hotmail.com>
2022-06-09 22:48:12 +0100

    Added react router
    Can be used to switch between pages. Added a few mock pages to verify
    was working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import { BrowserRouter, Routes, Route, Outlet, Link, To } from "react-router-dom";
 import styles from '../css/title.module.css'
+import About from "../pages/about"
+import Projects from "../pages/projects"
 
 function Title() {
   return <div className={styles.titleContainer}>
@@ -9,28 +12,48 @@ function Title() {
 }
 
 type OptionProps = {
+  route: To,
   children: React.ReactNode,
 }
 
 function Option(props: OptionProps) {
   return <li className={styles.option}>
-      {props.children}
+      <Link to={props.route}>{props.children}</Link>
   </li>
 }
 
 function Options() {
   return <ul className={styles.options}>
-    <Option>About.</Option>
-    <Option>Projects.</Option>
-    <Option>CV.</Option>
-    <Option>Contact.</Option>
+    <Option route="/about">About.</Option>
+    <Option route="/projects">Projects.</Option>
+    <Option route="/">CV.</Option>
+    <Option route="/">Contact.</Option>
   </ul>
 }
 
-
-export const App = () => (
-  <div className={styles.centralCol}>
+const Home = () => {
+  return <div className={styles.centralCol}>
     <Title />
     <Options />
   </div>
+}
+
+const Layout = () => {
+  return <div>
+    Layout
+    <Outlet />
+  </div>
+}
+
+export const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
