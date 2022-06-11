49ad46ac8dd196fd3b67869d48464e5eaae2e737
ehockedy <edhockedy@hotmail.com>
2022-06-11 14:21:17 +0100

    Started work on navigation bar
    Mace use of react router to make it a permanent fixture as part of the
    layout component. Added basic interaction styling.

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import styles from '../css/layout.module.css'
 
 const Layout = () => {
-    return <div>
-      Layout
+    return <>
+      <div className={styles.sidebar}>
+        <div className={styles.logoContainer}>
+          <div className={styles.logo}>
+            EH
+          </div>
+        </div>
+      </div>
       <Outlet />
-    </div>
+    </>
   }
 
 export default Layout
