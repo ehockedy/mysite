0203b0889d409ee6e9d523a306407c8d1fcf3aee
ehockedy <edhockedy@hotmail.com>
2022-07-04 19:49:39 +0100

    Updated font and background colour

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import { App } from "./App";
+import "@fontsource/montserrat"
 import '../css/index.css'
 
 const container = document.getElementById('app')
