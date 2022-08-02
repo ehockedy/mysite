d32ba6a1a405a0b7f290d5e5cae0931ea0991c3d
ehockedy <edhockedy@hotmail.com>
2022-08-02 21:05:59 +0100

    Removed old project card

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -7,32 +7,6 @@ import classnames from "classnames"
 const ProjectCard = () => {
     return <div className={styles.cardOuter}>
         <div className={styles.cardInner}>
-            <div className={classnames(styles.background, styles.cardFront)}>
-                <div className={classnames(styles.cardMain, styles.content)}>
-                    <img src={testImg} className={styles.circularImg}></img>
-                    <div className={classnames(styles.title, styles.contentLine)}>Defence of Karragarra Valley</div>
-                    <div className={classnames(styles.technologies, styles.contentLine)}>
-                        <span>node.js</span>
-                        <span>React</span>
-                        <span>PIXI js</span>
-                    </div>
-                    <div className={classnames(styles.description, styles.contentLine)}>
-                        A competitive and cooperative browsed based tower defence game.
-                    </div>
-                </div>
-            </div>
-            <div className={classnames(styles.background, styles.cardBack)}>
-                <div className={classnames(styles.cardExtraInfo, styles.content)}>
-                    hello
-                </div>
-            </div>
-        </div>
-    </div>
-}
-
-const ProjectCard2 = () => {
-    return <div className={styles.cardOuter}>
-        <div className={styles.cardInner}>
             <div className={classnames(styles.background, styles.cardFront)}
                 style={{
                     backgroundImage: `url(${dokvCardImg})`
@@ -53,4 +27,4 @@ const ProjectCard2 = () => {
     </div>
 }
 
-export default ProjectCard2;
+export default ProjectCard
