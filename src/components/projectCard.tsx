4446f6b139e690ae0ba52d7dad8fd18a746dc87b
ehockedy <edhockedy@hotmail.com>
2022-08-03 13:09:55 +0100

    Made project card reusable

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import testImg from "../assets/test.jpg"
-import dokvCardImg from "../assets/dokv_card.png"
 import styles from "../css/projectCard.module.css"
 import classnames from "classnames"
 
@@ -23,32 +21,34 @@ const ProjectCardBack = (props: ProjectCardBackProps) => {
     </>
 }
 
-const ProjectCard = () => {
+export type ProjectCardProps = {
+    projectName: string;
+    projectSubtitle: string;
+    backgroundImage: string;
+    aboutText: string;
+    dateRange: string;
+    link: string;
+}
+const ProjectCard = (props: ProjectCardProps) => {
     return <div className={styles.cardOuter}>
         <div className={styles.cardInner}>
             <div className={classnames(styles.background, styles.cardFront)}
                 style={{
-                    backgroundImage: `url(${dokvCardImg})`
+                    backgroundImage: `url(${props.backgroundImage})`
                 }}
             >
                 <div className={styles.nameBanner}>
                     <div className={styles.projectName}>
-                        Defence of Karragarra Valley
+                        {props.projectName}
                     </div>
                     <div className={styles.projectDescriptionShort}>
-                        Multiplayer tower defence game
+                        {props.projectSubtitle}
                     </div>
                 </div>
             </div>
             <div className={classnames(styles.background, styles.cardBack)}>
-                <ProjectCardBack dateRange="Jun 2020 - Apr 2022" link="https://google.com">
-                    After playing multiple online browser games at the start of the pandemic, I decide to 
-                    revisit an old favourite - Bloons Tower Defence. As I revelled in nostlagia, a thought struck me: what if I could play 
-                    this with friends? I searched, but could not find any multiplayer tower defence games. On top of an existing desire to make a multiplayer
-                    web app, I began this project. The more I developed, the more I
-                    enjoyed planning the mechanics, creating the sprites, and balancing the game. As such, the project lasted far longer than I ever intended,
-                    and even became a bit of a burden towards the end. Even so, I'm proud to have completed the first cooperative and colaborative
-                    tower defence game that I have ever played.
+                <ProjectCardBack dateRange={props.dateRange} link={props.link}>
+                    {props.aboutText}
                 </ProjectCardBack>
             </div>
         </div>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
 import { PageProps, PageTitle } from "../components/page";
-import ProjectCard from "../components/projectCard"
+import ProjectCard, { ProjectCardProps } from "../components/projectCard"
 import styles from "../css/projects.module.css"
+import dokvCardImg from "../assets/dokv_card.png"
+
+const projects: Array<ProjectCardProps> = [
+  {
+    projectName: 'Defence of Karragarra Valley',
+    projectSubtitle: 'Multiplayer tower defence game',
+    backgroundImage: dokvCardImg,
+    aboutText: "After playing multiple online browser games at the start of the pandemic, \
+      I decide to revisit an old favourite - Bloons Tower Defence. As I revelled in nostlagia,\
+      a thought struck me: what if I could play this with friends? I searched, but could not \
+      find any multiplayer tower defence games. On top of an existing desire to make a multiplayer \
+      web app, I began this project. The more I developed, the more I enjoyed planning the \
+      mechanics, creating the sprites, and balancing the game. As such, the project lasted far \
+      longer than I ever intended, and even became a bit of a burden towards the end. Even so,\
+      I'm proud to have completed the first cooperative and colaborative tower defence game that\
+      I have ever played.",
+    dateRange: 'Jun 2020 - Apr 2022',
+    link: 'https://google.com'  // TODO update this when ready
+  }
+]
 
 const Projects = (props: PageProps) => {
   return <div id={props.anchor}>
       <PageTitle value="Projects"></PageTitle>
       <div className={styles.projectCardContainer}>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
+        {projects.map((project, idx) => <ProjectCard {...project} key={`project${idx}`}/>)}
       </div>
     </div>
 };
